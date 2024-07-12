1927cf0b7a702dab019a19e35ce40f69b3c78181
menacingsoul <adarsh14103@gmail.com>
2024-07-12 22:12:17 +0530

    trying adsense-6

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,18 +8,12 @@ const DashboardLayout = ({ children }: Props) => {
   
 
   return (
-    <html>
-      <meta name="google-adsense-account" content="ca-pub-5673721717655381"/>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5673721717655381"
-     crossOrigin="anonymous"></script>
        <div className="h-screen w-screen flex flex-col md:flex-row">
       <SidebarToggle />
       <div className="md:ml-[300px] h-full flex-1">
         <div className="h-full overflow-auto">{children}</div>
       </div>
     </div>
-    </html>
-   
   );
 };
 
--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -8,16 +8,12 @@ const DashboardLayout = ({ children }: Props) => {
   
 
   return (
-    <html>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5673721717655381"
-     crossOrigin="anonymous"></script>
       <div className="h-screen w-screen flex flex-col md:flex-row">
       <SidebarToggle />
       <div className="md:ml-[300px] h-full flex-1">
         <div className="h-full overflow-auto">{children}</div>
       </div>
     </div>
-    </html>
     
   );
 };
